4dd34fe218f42dcfa25737ba8d61e2a594ba8863
Keith Perkins <keith.perkins@cnu.edu>
2021-10-21 23:04:28 -0400

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/DataVM.java
@@ -0,0 +1,198 @@
+package com.example.myapplication;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class DataVM extends ViewModel {
+
+    //gotta define this somewhere
+    String links[] = {  "https://www.pcs.cnu.edu/~kperkins/pets/p33.png",
+            "https://www.pcs.cnu.edu/~kperkins/pets/p44.png",
+            "https://www.pcs.cnu.edu/~kperkins/pets/p55.png",
+            "https://www.pcs.cnu.edu/~kperkins/pets/p22.png"};
+
+    String jsonlink="https://www.pcs.cnu.edu/~kperkins/pets/pets.json";
+
+    //this is the value that we want to keep track of through rotations
+    int currentLink=0;
+
+    GetImageThread myGetImageThread;
+    GetTextThread myGetTextThread;
+
+    //lets add some livedata
+    //the bitmap we are looking for
+    private MutableLiveData<Bitmap> bmp ;
+    public MutableLiveData<Bitmap> getbmp() {
+        if(bmp==null)
+            bmp=new MutableLiveData<Bitmap>();
+        return bmp;
+    }
+
+    //any communications from thread
+    private MutableLiveData<String> result ;
+    public MutableLiveData<String> getresult() {
+        if(result==null)
+            result=new MutableLiveData<String>();
+        return result;
+    }
+
+    public void getJSON(){
+        myGetTextThread=new GetTextThread(jsonlink);
+        myGetTextThread.start();
+    }
+    public void getImage(String url){
+        myGetImageThread=new GetImageThread(url);
+        myGetImageThread.start();
+    }
+
+    public class GetTextThread extends Thread{
+        private static final String     TAG = "GetTextThread";
+        private static final int        DEFAULTBUFFERSIZE = 8096;
+        private static final int        TIMEOUT = 1000;    // 1 second
+        protected int                   statusCode = 0;
+        private String                  url;
+
+        public GetTextThread(String url) {
+            this.url=url;
+        }
+
+        public void run() {
+            try {
+                URL url1 = new URL(url);
+
+                // this does no network IO
+                HttpURLConnection connection = (HttpURLConnection) url1.openConnection();
+
+                // can further configure connection before getting data
+                // cannot do this after connected
+                connection.setRequestMethod("GET");
+                connection.setReadTimeout(TIMEOUT);
+                connection.setConnectTimeout(TIMEOUT);
+                connection.setRequestProperty("Accept-Charset", "UTF-8");
+
+                // wrap in finally so that stream bis is sure to close
+                // and we disconnect the HttpURLConnection
+                BufferedReader in = null;
+                try {
+
+                    // this opens a connection, then sends GET & headers
+                    connection.connect();
+
+                    // lets see what we got make sure its one of
+                    // the 200 codes (there can be 100 of them
+                    // http_status / 100 != 2 does integer div any 200 code will = 2
+                    statusCode = connection.getResponseCode();
+                    if (statusCode / 100 != 2) {
+                        result.postValue("Failed! Statuscode returned is " + Integer.toString(statusCode));
+                        return;
+                    }
+
+                    in = new BufferedReader(new InputStreamReader(connection.getInputStream()), DEFAULTBUFFERSIZE);
+
+                    // the following buffer will grow as needed
+                    String myData;
+                    StringBuffer sb = new StringBuffer();
+
+                    while ((myData = in.readLine()) != null) {
+                        sb.append(myData);
+                    }
+                    result.postValue(sb.toString());
+                } finally {
+                    // close resource no matter what exception occurs
+                    in.close();
+                    connection.disconnect();
+                }
+            } catch (Exception exc) {
+                Log.d(TAG, exc.toString());
+                result.postValue(exc.toString());
+            }
+        }
+
+    }
+    public class GetImageThread extends Thread{
+        private static final String TAG = "GetImageThread";
+        private static final int        DEFAULTBUFFERSIZE = 50;
+        private static final int        NODATA = -1;
+        private int                     statusCode=0;
+        private String                  url;
+
+        public GetImageThread(String url) {
+            this.url=url;
+        }
+
+        public void run(){
+            // note streams are left willy-nilly here because it declutters the
+            // example
+            try {
+                URL url1 = new URL(url);
+
+                // this does no network IO
+                HttpURLConnection connection = (HttpURLConnection) url1.openConnection();
+
+                // can further configure connection before getting data
+                // cannot do this after connected
+                // connection.setRequestMethod("GET");
+                // connection.setReadTimeout(timeoutMillis);
+                // connection.setConnectTimeout(timeoutMillis);
+
+                // this opens a connection, then sends GET & headers
+                connection.connect();
+
+                // lets see what we got make sure its one of
+                // the 200 codes (there can be 100 of them
+                // http_status / 100 != 2 does integer div any 200 code will = 2
+                int statusCode = connection.getResponseCode();
+
+                if (statusCode / 100 != 2) {
+                    result.postValue("Failed! Statuscode returned is " + Integer.toString(statusCode));
+                    return;
+                }
+
+                // get our streams, a more concise implementation is
+                // BufferedInputStream bis = new
+                // BufferedInputStream(connection.getInputStream());
+                InputStream is = connection.getInputStream();
+                BufferedInputStream bis = new BufferedInputStream(is);
+
+                // the following buffer will grow as needed
+                ByteArrayOutputStream baf = new ByteArrayOutputStream(DEFAULTBUFFERSIZE);
+                int current = 0;
+
+                // wrap in finally so that stream bis is sure to close
+                try {
+                    while ((current = bis.read()) != NODATA) {
+                        baf.write((byte) current);
+                    }
+
+                    // convert to a bitmap
+                    byte[] imageData = baf.toByteArray();
+                    //some live data here
+                    //can only postValue from background thread not setValue
+                    bmp.postValue(BitmapFactory.decodeByteArray(imageData, 0, imageData.length));
+                    result.postValue(url);
+
+                } finally {
+                    // close resource no matter what exception occurs
+                    bis.close();
+                }
+            } catch (Exception exc) {
+                Log.d(TAG, exc.toString());
+                result.postValue(exc.toString());
+            }
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,99 @@
+package com.example.myapplication;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.ui.AppBarConfiguration;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    //persists accross config changes
+    DataVM myVM;
+    ImageView iv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        iv=findViewById(R.id.imageView1);
+        setSupportActionBar(findViewById(R.id.toolbar));
+
+        // Create a ViewModel the first time the system calls an activity's
+        // onCreate() method.  Re-created activities receive the same
+        // MyViewModel instance created by the first activity.
+        myVM = new ViewModelProvider(this).get(DataVM.class);
+
+        // Create the observer which updates the UI.
+        final Observer<Bitmap> bmpObserver = new Observer<Bitmap>() {
+            @Override
+            public void onChanged(@Nullable final Bitmap newbmp) {
+                // Update the UI, in this case, a TextView.
+                iv.setImageBitmap(newbmp);
+             }
+        };
+        // Observe the LiveData, passing in this activity as the LifecycleOwner and the observer.
+        myVM.getbmp().observe(this,bmpObserver);
+
+        // Create the observer which updates the UI.
+        final Observer<String> resultObserver = new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable final String result) {
+                // Update the UI, in this case, a TextView.
+                Toast.makeText(MainActivity.this,result,Toast.LENGTH_SHORT).show();
+            }
+        };
+        // Observe the LiveData, passing in this activity as the LifecycleOwner and the observer.
+        myVM.getresult().observe(this,resultObserver);
+
+        findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url=myVM.links[myVM.currentLink++%myVM.links.length];
+                myVM.getImage(url);
+            }
+        });
+
+        findViewById(R.id.fabgetjson).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myVM.getJSON();
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
